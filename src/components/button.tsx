8d84f68f5573bffe0f244fad12d8a274078f97cc
Masood-ul-Rehman <masooddulrehman@gmail.com>
2025-01-14 16:27:19 +0500

    updated button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const Button = ({ text }: { text: string }) => {
   return (
-    <div className=" flex justify-center bg-primary items-center shadow-customShadow">
-      <button className="bg-customGradient rounded-xl relative px-[1px] py-[1px] backdrop-blur-[50px]">
-        <div className="bg-[#09132F] px-[18px] py-[11px] rounded-xl ">
-          <span className="text-white text-sm font-bold">{text}</span>
-        </div>
-      </button>
-    </div>
+    <button className="bg-customGradient rounded-xl relative px-[1px] py-[1px] backdrop-blur-[50px]">
+      <div className="bg-[#09132F] px-[22px] py-[10px] rounded-xl ">
+        <span className="text-white text-sm font-bold ">{text}</span>
+      </div>
+    </button>
   );
 };
 
