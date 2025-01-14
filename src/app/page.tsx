02d1bfa95be280472295db0d473cd3ff6faff5ee
Masood-ul-Rehman <masooddulrehman@gmail.com>
2025-01-14 16:08:03 +0500

    updated ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Button from "@/components/button";
 import CardItems from "@/components/card-items";
-
 export default function Home() {
   return (
     <div className="p-4 bg-white flex flex-col items-center gap-4 text-white">
       <div className="bg-[rgb(8,18,46)] w-full h-[300px] p-6 rounded-xl">
-        <div className=" flex justify-between w-full">
+        <div className=" flex justify-between items-center w-full">
           <Image src="/logo.png" alt="logo" width={140} height={100} />
           <img src="/open.png" alt="open" />
         </div>
@@ -30,42 +29,67 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-full flex flex-col gap-2 bg-[rgb(8,18,46)] p-4 rounded-xl gap-4">
+      <div className="w-full flex flex-col  bg-[rgb(8,18,46)] p-4 rounded-xl gap-4">
         <div className="flex  gap-3">
           <div className=" flex gap-2 items-center">
-            <img src="/seman.png" alt="" />
+            <Image src="/seman.png" alt="semen icon" width={20} height={20} />
             <h4 className="text-md font-medium">Semen</h4>
             <h4 className="text-sm font-medium">Sample 19A43A</h4>
           </div>
           <div className=" flex gap-2 items-center bg-[#5577FF33] rounded-xl px-2 py-1">
-            <img src="/avatar.png" alt="" />
+            <Image src="/avatar.png" alt="avatar" width={16} height={16} />
             <h4 className="text-sm font-medium text-[#5577FF]">
               Partner: PESA Testicular
             </h4>
           </div>
           <div className=" flex gap-2 items-center bg-[#5577FF33] rounded-xl px-2 py-1">
-            <img src="/snow.png" alt="" />
+            <Image src="/snow.png" alt="snow icon" width={16} height={16} />
             <h4 className="text-sm font-medium text-[#5577FF]">Frozen</h4>
           </div>
           <div className=" flex gap-2 items-center bg-[#5577FF33] rounded-xl px-2 py-1">
-            <img src="/tube.png" alt="" />
+            <Image src="/tube.png" alt="tube icon" width={10} height={10} />
             <h4 className="text-sm font-medium text-[#5577FF]">12:56 PM</h4>
             <h4 className="text-sm font-medium text-[#5577FF]">10 Jun 2024</h4>
           </div>
         </div>
         <div className="w-full grid grid-cols-3 gap-3">
-          <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
+          <div className="bg-[#FFFFFF0D] p-4 rounded-xl gap-2 flex flex-col">
             <div className="flex gap-2 items-center">
-              <img src="/research.png" alt="" />
+              <Image src="/research.png" alt="" width={16} height={16} />
               <h4 className="text-sm font-medium">Pre-processing</h4>
             </div>
             <CardItems />
           </div>
-          <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
-            <h4 className="text-md font-medium">Semen</h4>
+          <div className="bg-[#FFFFFF0D] p-4 rounded-xl gap-2 flex flex-col">
+            <div className="flex gap-2 items-center ">
+              <Image
+                src="/calculator.png"
+                alt="calculator icon"
+                width={18}
+                height={17}
+              />
+              <h4 className="text-sm font-medium">Post Preparation</h4>
+
+              <div className="bg-[#5577FF33] rounded-full px-2 py-1">
+                <h4 className="text-sm font-medium text-[#5577FF]">
+                  microfluidics{" "}
+                </h4>
+              </div>
+            </div>{" "}
+            <CardItems />
           </div>
-          <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
-            <h4 className="text-md font-medium">Semen</h4>
+          <div className="bg-[#FFFFFF0D]  rounded-xl  ">
+            <div className="bg-[url('/blur.png')] bg-cover bg-center bg-no-repeat w-full h-full flex flex-col items-center justify-center gap-8 p-4">
+              <div className="text-center flex gap-2 flex-col">
+                <h4 className="text-md font-medium">
+                  No insemination details yet
+                </h4>
+                <p className="text-sm font-medium text-[#929DB7]">
+                  This will come after OPU has been verified
+                </p>
+              </div>
+              <Button text="Add insemination details" width="w-[220px]" />
+            </div>
           </div>
         </div>
       </div>
--- src/components/card-items.tsx
+++ src/components/card-items.tsx
@@ -6,8 +6,8 @@ const items = [
     value: "15 ml",
   },
   {
-    title: "15 M/mL",
-    value: "Concentration",
+    title: "Concentration",
+    value: "15 M/mL",
   },
   {
     title: "4%",
@@ -21,16 +21,24 @@ const items = [
 
 const CardItems = () => {
   return (
-    <div className="flex items-center gap-2">
-      {items.map((item) => (
-        <div
-          className="flex flex-col gap-1 p-4 rounded-md bg-[#FFFFFF0D]"
-          key={item.value}
-        >
-          <h1 className="text-md font-medium text-white">{item.value}</h1>
-          <h1 className="text-xs font-medium text-[#929DB7]">{item.title}</h1>
-        </div>
-      ))}
+    <div className="flex flex-col items-center gap-2">
+      <div className="grid grid-cols-2 gap-2 w-full">
+        {items.map((item) => (
+          <div
+            className="flex flex-col gap-1 p-4 rounded-md bg-[#FFFFFF0D]"
+            key={item.value}
+          >
+            <h1 className="text-md font-medium text-white">{item.value}</h1>
+            <h1 className="text-xs font-medium text-[#929DB7]">{item.title}</h1>
+          </div>
+        ))}
+      </div>
+      <div className="flex  gap-2 p-3 rounded-md bg-[#FFFFFF0D] w-full">
+        <img src="/file-02.png" alt="" />
+        <h1 className="text-xs font-medium text-white">
+          Minimal bleeding noted at puncture site, reso.....
+        </h1>
+      </div>
     </div>
   );
 };
